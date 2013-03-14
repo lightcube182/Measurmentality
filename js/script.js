17dfb773f797c7ffb6f567331b6073e7e80a8d5c
Ben Koury <lightcube182@gmail.com>
2013-03-14 17:15:15 -0400

    Added in octave control, but not working.

--- js/script.js
+++ js/script.js
@@ -8,11 +8,10 @@
 var stage;
 var size = 50;
 var fKey = 125;
-var eKey = 125 + size*.5;
-var dKey = 125 + size;
-var cKey = 125 + size*1.5;
-var bKey = 125 + size*2;
-var aKey = 125 + size*2.5;
+var dKey = 125 + size*4.5;
+var cKey = 125 + size*5;
+var bKey = 125 + size*5.5;
+var aKey = 125 + size*6;
 var gKey = 125 + size*3;
 var eKey = 125 + size*4;
 var gClef;
@@ -27,16 +26,16 @@ function init(){
     staff = new createjs.Shape();
     staff.graphics.beginStroke("black");
     staff.graphics.setStrokeStyle(5);
-    staff.graphics.moveTo(0, eKey);
-    staff.graphics.lineTo(canvas.width, eKey);
-    staff.graphics.moveTo(0, gKey);
-    staff.graphics.lineTo(canvas.width, gKey);
-    staff.graphics.moveTo(0, bKey);
-    staff.graphics.lineTo(canvas.width, bKey);
-    staff.graphics.moveTo(0, dKey);
-    staff.graphics.lineTo(canvas.width, dKey);
-    staff.graphics.moveTo(0, fKey);
-    staff.graphics.lineTo(canvas.width, fKey);
+    staff.graphics.moveTo(0, 125 + size*4);
+    staff.graphics.lineTo(canvas.width, 125 + size*4);
+    staff.graphics.moveTo(0, 125 + size*3);
+    staff.graphics.lineTo(canvas.width, 125 + size*3);
+    staff.graphics.moveTo(0, 125 + size*2);
+    staff.graphics.lineTo(canvas.width, 125 + size*2);
+    staff.graphics.moveTo(0, 125 + size);
+    staff.graphics.lineTo(canvas.width, 125 + size);
+    staff.graphics.moveTo(0, 125);
+    staff.graphics.lineTo(canvas.width, 125);
     stage.addChild(staff);
     
     gClef = new createjs.Bitmap("images/TrebleClef.svg.hi.png");
@@ -46,15 +45,16 @@ function init(){
     stage.addChild(gClef);
     
     stage.update();
-    generateNote(eKey, 0);
+    generateNote(dKey, 3);
 }
 
 document.onkeypress = function(evt) {
     evt = evt || window.event;
     var charCode = evt.keyCode || evt.which;
+    var octave;
     switch (charCode) {
         case 97:
-            gClef.y = aKey;
+            gClef.y = aKey - (octave - 3)*7*25;
             stage.update();
             break;
         case 32:
@@ -67,28 +67,43 @@ document.onkeypress = function(evt) {
             stage.update();
             break;
         case 115:
-            gClef.y = bKey;
+            gClef.y = bKey - ((octave - 3)*7*25);
             stage.update();
             break;
         case 100:
-            gClef.y = cKey;
+            gClef.y = cKey - ((octave - 3)*7*25);
             stage.update();
             break;
         case 102:
-            gClef.y = dKey;
+            gClef.y = dKey - ((octave - 3)*7*25);
             stage.update();
             break;
         case 106:
-            gClef.y = eKey;
+            gClef.y = eKey - ((octave - 3)*7*25);
             stage.update();
             break;
         case 107:
-            gClef.y = fKey;
+            gClef.y = fKey - ((octave - 3)*7*25);
             stage.update();
             break;
         case 108:
-            gClef.y = gKey;
+            gClef.y = gKey - ((octave - 3)*7*25);
             stage.update();
+            break;
+        case 49:
+            octave = 1;
+            break;
+        case 50:
+            octave = 2;
+            break;
+        case 51:
+            octave = 3;
+            break;
+        case 52:
+            octave = 4;
+            break;
+        case 53:
+            octave = 5;
             break;
         default:
             break;
@@ -101,10 +116,10 @@ function generateNote(letter, octave){
     var theNote = new createjs.Bitmap("images/quarter_note.svg");
     notes.push(theNote);
     theNote.regY = 750;
-    theNote.scaleX = .1;
-    theNote.scaleY = .1;
+    theNote.scaleX = .2;
+    theNote.scaleY = .2;
     theNote.x = canvasRight;
-    theNote.y = letter + (octave * 7);
+    theNote.y = letter - ((octave - 3)*7*25);
     stage.addChild(theNote);
     createjs.Ticker.setFPS(60);
     stage.update();
